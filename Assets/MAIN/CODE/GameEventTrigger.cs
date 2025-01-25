99476413097fedff3bb43fd965879d3d5835b4c3
rojo1123 <ronaldo.jonson@netcracker.com>
2025-01-25 08:57:11 -0300

    fixed respawn and added hazards

--- Assets/MAIN/CODE/GameEventTrigger.cs
+++ Assets/MAIN/CODE/GameEventTrigger.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System;
+
+[Serializable]
+public class CustomEvent: UnityEvent<Transform> 
+{
+
+}
 
 public class GameEventTrigger : MonoBehaviour
 {
-    public UnityEvent<Transform> onPlayerEnterEvent;
+    public CustomEvent onPlayerEnterEvent;
     public UnityEvent onBubbleEnterEvent;
 
     void OnTriggerEnter(Collider other)
     {
+        Debug.Log(other);
+        Debug.Log(onPlayerEnterEvent);
+
         if(other.CompareTag("Player"))
             onPlayerEnterEvent.Invoke(other.transform);
 
