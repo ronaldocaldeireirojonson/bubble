7f28cd03d8186c70936d5e29b36f8656adae9a6e
rojo1123 <ronaldo.jonson@netcracker.com>
2025-01-26 00:10:11 -0300

    STABLE

--- Assets/MAIN/CODE/ITEMS/BlowGun.cs
+++ Assets/MAIN/CODE/ITEMS/BlowGun.cs
@@ -2,35 +2,36 @@ using UnityEngine;
 
 public class BlowGun : Item
 {
-    public GameObject arrow;
-
-    public Vector3 pointA;
-    public Vector3 pointB;
-    public Material planeMaterial;
-
-    public float forwardOffset = 1;
-    public float overlapRadius = 2;
+    public float overlapRadius = 1;
     public float maxForce = 5;
-    public float forceModifier = 1;
-    public float arrowVisualMultiplier = 5;
-
-    GameObject plane;
+    public float windUpSpeed = .2f;
+    public AnimationCurve curve;
     float force = 0;
+    float percent = 0;
+
+    public override void Setup()
+    {
+        quad = Object.Instantiate(quadPrefab);
+    }
 
     public override void Hold(Transform caster)
     {
-        force += Time.deltaTime;
+        percent += Time.deltaTime * windUpSpeed;
+        force = curve.Evaluate(percent) * maxForce;
         force = Mathf.Min(force, maxForce);
-        arrow.transform.rotation = caster.rotation;
-        arrow.transform.position = caster.position + caster.forward * force * arrowVisualMultiplier;
+
+        quad.transform.rotation = caster.rotation;
+        quad.transform.position = caster.position;
+        quad.transform.localScale = new Vector3(1, 1, force);
     }
 
     public override void Release(Transform caster)
     {
         force = Mathf.Min(force, maxForce);
-        arrow.transform.position = new Vector3(9999, 9999, 9999);
+        quad.transform.position = new Vector3(9999, 9999, 9999);
 
-        Collider[] hitColliders = Physics.OverlapSphere(caster.position + caster.forward * forwardOffset, overlapRadius);
+        Collider[] hitColliders = Physics.OverlapCapsule(caster.position, caster.position + (caster.forward * force), overlapRadius);
+
         IPushable pushable = null;
 
         foreach (Collider hit in hitColliders)
@@ -38,31 +39,13 @@ public class BlowGun : Item
             pushable = hit.GetComponent<IPushable>();
             if(pushable != null)
             {
-                Vector3 direction = (hit.transform.position - caster.position);
+                Vector3 direction = (hit.transform.position - caster.position).normalized;
                 direction.y = 0;
-                pushable.Push(direction * forceModifier * force);
+                pushable.Push(direction * force);
             }
         }
 
         force = 0;
-    }
-
-    void DrawPlane(Vector3 a, Vector3 b)
-    {
-        Vector3 center = (a + b) / 2f;
-        Vector3 direction = b - a;
-
-        if(plane == null)
-            plane = GameObject.CreatePrimitive(PrimitiveType.Plane);
-
-        plane.transform.position = center;
-
-        float distance = direction.magnitude;
-        plane.transform.localScale = new Vector3(distance / 10f, 1f, 0.1f);
-
-        if (planeMaterial != null)
-        {
-            plane.GetComponent<Renderer>().material = planeMaterial;
-        }
+        percent = 0;
     }
 }
