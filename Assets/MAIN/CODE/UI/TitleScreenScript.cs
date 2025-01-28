d6ab4482393b8a39327dcbbd40cfc70d98616faf
VtrU8i <60532911+VtrU8i@users.noreply.github.com>
2025-01-28 18:23:03 -0300

    UI TitleScreen Fix
    Had to turn off the Airconsole prefab, cuz was openning the website

---
+++ Assets/MAIN/CODE/UI/TitleScreenScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenController : MonoBehaviour
+{
+    // The name of the main menu scene you want to load
+    public string mainMenuSceneName = "Menu";
+
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for any button press (or mouse click)
+        if (Input.anyKeyDown)
+        {
+            LoadMainMenu();
+        }
+    }
+
+    // Function to load the main menu scene
+    public void LoadMainMenu()
+    {
+        StartCoroutine(LoadMenu(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadMenu (int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+}
